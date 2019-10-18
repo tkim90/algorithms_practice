b06267de06ee9119406491b49be75815ded07e8e
TK <taehnkim@gmail.com>
2019-10-18 16:00:21 -0700

    Implemented stockPriceMaxProfit.js

---
+++ algorithms/greedy/stockPriceMaxProfit.js
@@ -0,0 +1,92 @@
+// I: arr, stockPrices
+// O: maxDiff between two arrays
+
+// const maxDiff = 0;
+// const stockPrices = [10, 7, 5, 8, 11, 9];
+//                      ^             ^
+// max btwn 7 - 10 and 0 : 6
+//  [9, 7, 4, 1] -2
+
+function getMaxProfit(stockPrices) {
+  if (stockPrices.length <= 1) {
+    throw new Error('Getting a profit requires at least 2 prices');
+  }
+
+  let minPrice = stockPrices[0];
+  let maxProfit = stockPrices[1] - stockPrices[0];
+
+  for (let i = 1; i < stockPrices.length; i++) {
+    maxProfit = Math.max(maxProfit, stockPrices[i] - minPrice);
+    minPrice = Math.min(minPrice, stockPrices[i]);
+  }
+  return maxProfit;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'price goes up then down';
+let actual = getMaxProfit([1, 5, 3, 2]);
+let expected = 4;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes down then up';
+actual = getMaxProfit([7, 2, 8, 9]);
+expected = 7;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes up all day';
+actual = getMaxProfit([1, 6, 7, 9]);
+expected = 8;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes down all day';
+actual = getMaxProfit([9, 7, 4, 1]);
+expected = -2;
+assertEqual(actual, expected, desc);
+
+desc = 'price stays the same all day';
+actual = getMaxProfit([1, 1, 1, 1]);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'error with empty prices';
+const emptyArray = () => (getMaxProfit([]));
+assertThrowsError(emptyArray, desc);
+
+desc = 'error with one price';
+const onePrice = () => (getMaxProfit([1]));
+assertThrowsError(onePrice, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
