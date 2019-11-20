2802b3aa4050124941d86101428bce94343c604d
TK <taehnkim@gmail.com>
2019-11-19 21:21:16 -0800

    Added an arrays version of permutations1.js

--- arraysStrings/permutations1.js
+++ arraysStrings/permutations1.js
@@ -22,3 +22,32 @@ var permute = function(nums) {
 
 // console.log(permute('abc'));
 // console.log(permute('abcd'));
+
+
+// --- ARRAY version
+
+var permute2 = function(nums) {
+  let output = [];
+  
+  const helper = (current, options) => {
+    if (current.length === nums.length) {
+      output.push(current.slice());
+      return ;
+    }
+    
+    for (let i = 0; i < options.length; i++) {
+      let choice = options[i];
+      let newOptions = options.slice(0, i).concat(options.slice(i + 1, nums.length));
+      current.push(choice);
+      helper(current, newOptions);
+      current.pop();
+    }
+  };
+  
+  helper([], nums);
+  
+  return output;
+};
+
+console.log(permute2([1,2,3]));
+console.log(permute2([1,3,6,9]));
